a2dc9219ad3800d6745714e12848581d2549c109
rafael <koder1554@gmail.com>
2022-07-31 18:38:45 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+
+def database_insert(data):
+    with open("database.csv", mode="a") as database:
+        email = data.get("email")
+        subject = data.get("subject")
+        message = data.get("message")
+        csv_writer = csv.writer(database, lineterminator="\n", delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+class Server:
+    def __init__(self):
+        self._app = Flask(__name__)
+        self._init__routes()
+
+    def _init__routes(self):
+        @self._app.route("/<string:page_name>")
+        def index(page_name):
+            return render_template(page_name)
+
+        @self._app.route("/contact", methods=["POST"])
+        def contact():
+            data = request.form.to_dict()
+            database_insert(data)
+            return redirect("/thankyou.html")
+
+    def get_app(self):
+        return self._app
+
+
+app = Server().get_app()
